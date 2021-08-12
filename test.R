95a50893f024eabe3910026a667de3809832b6a6
chang0-0 <bigyoung8375@naver.com>
2021-08-12 18:39:55 +0900

    complete

---
+++ test.R
@@ -0,0 +1,30 @@
+cust_id <- c("c01","c02","c03","c04") 
+last_name <- c("Kim", "Lee", "Choi", "Park") 
+cust_mart_1 <- data.frame(cust_id, last_name)
+
+cust_mart_2 <- data.frame( cust_id = c("c05", "c06", "c07"), last_name = c("Bae", "Kim", "Lim"))
+
+cust_mart_12 <- rbind(cust_mart_1, cust_mart_2)
+
+
+cust_mart_3 <- data.frame( cust_id = c("c08", "c09"), last_name = c("Lee", "Park"), gender = c("F", "M"))
+
+cust_mart_4 <- data.frame( cust_id = c("c10", "c11"), first_name = c("Kildong", "Yongpal"))
+
+cust_mart_5 <- data.frame( age = c(20, 25, 19, 40, 32, 39, 28), income = c(2500, 2700, 0, 7000, 3400, 3600, 2900))
+
+"+"(2,3)
+
+T_name <- c("T1","T2","T3","T4","T5","T6")
+x<-c(1.4, 1.8, 1.5, 1.4, 1.6, 1.5)
+y<-c(3.2, 3.4, 3.9, 3.2, 3.5, 3.9)
+
+
+dfm1 <- data.frame(T_name=c("T1","T2","T3","T4","T5","T6"), x=c(1.4, 1.8, 1.5, 1.4, 1.6, 1.5), y=c(3.2, 3.4, 3.9, 3.2, 3.5, 3.9))
+dfm2 <- data.frame(T_name=c("T1","T3","T5"), z=c(5.7, 5.8, 6.9))
+
+cbind(dfm1, dfm2, by="T_name")
+rbind(dfm1, dfm2, by="T_name")  #오류 발생
+merge(dfm1, dfm2, by="T_name")  
+subset(dfm1, dfm2, by="T_name") #오류 발생 
+#비어있는 칸은 재활용규칙에 의해서 반복적으로 입력됨
