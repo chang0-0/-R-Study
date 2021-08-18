5c130069f71122324639e67a5dbacc848120a484
chang0-0 <bigyoung8375@naver.com>
2021-08-19 00:42:03 +0900

    complete

---
+++ Foundation.R
@@ -0,0 +1,22 @@
+
+is_effective <- TRUE
+is_effective
+# TRUE
+
+class(is_effective)
+"logical"
+
+is_effective <- FALSE
+
+
+# ("서울", "부산", "제주")의 전체 범주 중 "서울" 저장
+sido <- factor("서울", c("서울", "부산", "제주"))
+sido
+
+levels(sido)
+# [1] "서울" "부산" "제주"
+
+a <- 10
+a[2] <- 20
+
+# [1] 10 20
