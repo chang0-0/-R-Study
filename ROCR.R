63e566901fc4e9528185b4b6d0559e4c38de28cc
chang0-0 <bigyoung8375@naver.com>
2021-08-24 16:20:09 +0900

    complete

---
+++ ROCR.R
@@ -0,0 +1,14 @@
+install.packages("binary classification")
+install.packages("party")
+install.packages("ROCR")
+
+library(party)
+library(rpart)
+library(ROCR)
+
+x <- kyphosis[sample(1:nrow(kyphosis), nrow(kyphosis), replace=F),]
+x.train <- kyphosis[1:floor(nrow(x)*0.75),]
+x.evaluate <- kyphosis[floor(nrow(x)*0.75):nrow(x),]
+x.model <- cforest(Kyphosis~Age+Number+Start, data=x.train)
+x.evaluate$prediction <- predict(x.model, newdata=x.evaluate)
+x.evaluate$correct <- x.evaluate$prediction == x.evaluate$Kyphosis
